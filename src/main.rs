56282052822ca41285ff2a6d09fbe734ba2438df
Matthias Wolf <m@sushinara.net>
2020-07-18 14:31:50 +0200

    Test implementation for systemd status.

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,18 @@ use embedded_graphics::Drawing;
 mod display;
 use display::Inky;
 
+mod service;
+use service::Services;
+
 const COLS: u16 = 400;
 const ROWS: u16 = 300;
 
 fn main() {
+    println!("{:?}", Services::new().expect("dbus").state("gdm"));
+    println!("{:?}", Services::new().expect("dbus").state("sshd"));
+    println!("{:?}", Services::new().expect("dbus").state("syncthing@matze"));
+    println!("{:?}", Services::new().expect("dbus").state("foob"));
+    println!("{:?}", Services::new().expect("dbus").state("mpd"));
     let mut black_buffer = [255u8; ROWS as usize * COLS as usize];
     let mut red_buffer = [0u8; ROWS as usize * COLS as usize];
     let mut inky = Inky::new(
@@ -31,6 +39,13 @@ fn main() {
                 .stroke(Some(Color::Black))
                 .fill(Some(Color::White))
                 .translate(Coord::new(1, 1))
+                .into_iter(),
+        );
+        d.draw(
+            ProFont9Point::render_str("We're somewhat useless right now")
+                .stroke(Some(Color::Red))
+                .fill(Some(Color::White))
+                .translate(Coord::new(1, 30))
                 .into_iter(),
         );
     })
---
+++ src/service.rs
@@ -0,0 +1,49 @@
+extern crate dbus;
+
+use dbus::blocking::stdintf::org_freedesktop_dbus::Properties;
+use dbus::blocking::Connection;
+use dbus::strings::Path;
+use std::time::Duration;
+
+#[derive(Debug)]
+pub enum Status {
+    Running,
+    Stopped,
+    Failed,
+    Unavailable,
+    Unknown,
+}
+
+pub struct Services {
+    conn: Connection,
+}
+
+impl Services {
+    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        let conn = Connection::new_system()?;
+        Ok(Services { conn })
+    }
+
+    pub fn state(&self, service: &str) -> Status {
+        self._state(service).unwrap_or(Status::Unavailable)
+    }
+
+    fn _state(&self, service: &str) -> Result<Status, Box<dyn std::error::Error>> {
+        let interface = Path::new(format!("/org/freedesktop/systemd1/unit/{}_2eservice", service.replace("@", "_40")))?;
+        let proxy = self.conn.with_proxy(
+            "org.freedesktop.systemd1",
+            interface,
+            Duration::from_millis(5000),
+        );
+        let load: String = proxy.get("org.freedesktop.systemd1.Unit", "LoadState")?;
+        let active: String = proxy.get("org.freedesktop.systemd1.Unit", "ActiveState")?;
+        let detail: String = proxy.get("org.freedesktop.systemd1.Unit", "SubState")?;
+        Ok(match (&load[..], &active[..], &detail[..]) {
+            ("loaded", "active", "running") => Status::Running,
+            ("loaded", "inactive", _) => Status::Stopped,
+            ("loaded", "failed", _) => Status::Failed,
+            ("not-found", _, _) => Status::Unavailable,
+            _ => Status::Unknown,
+        })
+    }
+}
